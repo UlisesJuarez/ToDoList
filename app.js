b40a4dcc09164da1e515023d40960d31179a6c93
UlisesJE <ujuareze1700@alumno.ipn.mx>
2022-01-22 23:02:56 -0600

    template with ejs

--- app.js
+++ app.js
@@ -1,21 +1,46 @@
-const express=require("express");
-const bodyParser=require("body-parser");
+const express = require("express");
+const bodyParser = require("body-parser");
 
-const app=express();
+const app = express();
 
-app.get("/",function(req,res){
-    var today=new Date();
-    var currentDay=today.getDay();
-    if(currentDay===6 || currentDay===0){
-        res.write("<h2>Yeat it's a weekend!</h2>")
-        res.write("<p>Enjoy the day</p>")
-        res.send();
-    }else{
-        res.send(__dirname+"/index.html");
-        
+app.set('view engine', 'ejs');
+
+
+app.get("/", function (req, res) {
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    switch (currentDay) {
+        case 0:
+            day = "Sunday";
+            break;
+        case 1:
+            day = "Monday";
+            break;
+        case 2:
+            day = "Tuesday";
+            break;
+        case 3:
+            day = "Wednesday";
+            break;
+        case 4:
+            day = "Thursday";
+            break;
+        case 5:
+            day = "Friday";
+            break;
+        case 6:
+            day = "Saturday";
+            break;
+
+        default:
+            console.log("Error current day is equal to: "+currentDay)
     }
+
+    res.render("list", { kindOfDay: day });
 });
 
-app.listen(3000,function(){
+app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
